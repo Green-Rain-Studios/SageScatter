daeca3d6c5b945e359c83c49829e8a183f4df9f5
aashish@greenrain.io <aashish@greenrain.io>
2023-08-03 11:25:04 +0530

    Added `RoadSplineActor` boilerplate

---
+++ Source/SageScatter/Private/RoadSplineActor.cpp
@@ -0,0 +1,26 @@
+﻿// 2023 Green Rain Studios
+
+
+#include "RoadSplineActor.h"
+
+
+// Sets default values
+ARoadSplineActor::ARoadSplineActor()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ARoadSplineActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ARoadSplineActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/SageScatter/Public/RoadSplineActor.h
@@ -0,0 +1,26 @@
+﻿// 2023 Green Rain Studios
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SplinePlacementActor.h"
+#include "RoadSplineActor.generated.h"
+
+UCLASS()
+class SAGESCATTER_API ARoadSplineActor : public ASplinePlacementActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ARoadSplineActor();
+	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+};
